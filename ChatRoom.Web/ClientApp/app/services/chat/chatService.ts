0b29c40b90a2fdc49f8ec27e959fa80bfe74d77c
Michał Nykiel <m.nykiel@weegree.com>
2017-11-08 11:22:15 +0100

    references fix and update

--- ChatRoom.Web/ClientApp/app/services/chat/chatService.ts
+++ ChatRoom.Web/ClientApp/app/services/chat/chatService.ts
@@ -33,7 +33,7 @@ export class ChatService {
 
         this.hubConnection = new HubConnection(this.httpConnection);
 
-        this.hubConnection.onClosed = e => { console.log(e); };
+        this.hubConnection.onclose = e => { console.log(e); };
 
         this.hubConnection.start().catch(err => console.log(err));
 
